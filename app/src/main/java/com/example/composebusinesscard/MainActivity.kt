da640a37baa1eff420b8ed30b8f0b642542c44b8
RanaHisham18 <hishamrana00@gmail.com>
2023-11-05 10:28:51 +0200

    make the card

--- app/src/main/java/com/example/composebusinesscard/MainActivity.kt
+++ app/src/main/java/com/example/composebusinesscard/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.composebusinesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composebusinesscard.ui.theme.ComposeBusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +30,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CreateBusinessCard()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateBusinessCard(){
+   Surface(modifier = Modifier
+       .fillMaxWidth()
+       .fillMaxHeight()) {
+Card(modifier = Modifier.width(200.dp)
+    .height(390.dp)
+    .padding(12.dp),
+    shape = RoundedCornerShape(corner = CornerSize(15.dp))
+) {
+
 }
+   }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeBusinessCardTheme {
-        Greeting("Android")
+        CreateBusinessCard()
     }
 }
